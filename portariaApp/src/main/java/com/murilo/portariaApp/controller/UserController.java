3949d8ec3701c1f51a89e87fa7b4cdcade8d1c18
Murilo <munarimurilo441@gmail.com>
2026-03-31 20:29:35 -0300

    metodo post criado na controller

--- portariaApp/src/main/java/com/murilo/portariaApp/controller/UserController.java
+++ portariaApp/src/main/java/com/murilo/portariaApp/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.murilo.portariaApp.controller;
 
+import com.murilo.portariaApp.Entity.User;
+import com.murilo.portariaApp.dto.user.UserRequestDTO;
 import com.murilo.portariaApp.repository.UserRepository;
+import com.murilo.portariaApp.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create (UserRequestDTO userRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userRequestDTO));
+    }
 
 
 }
--- portariaApp/src/main/java/com/murilo/portariaApp/service/UserService.java
+++ portariaApp/src/main/java/com/murilo/portariaApp/service/UserService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -34,6 +35,11 @@ public class UserService {
         users.setActive(true);
 
         return userRepository.save(users);
+    }
+
+    @Transactional
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 
 }
